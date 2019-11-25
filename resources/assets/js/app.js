8eacfae614b9683c845eccc78a8ab5ee85694e01
vagrant <vagrant@homestead>
2019-11-25 07:53:04 +0000

    feat: 更新前端资源

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,66 @@
-// babel-polyfill
-import 'babel-polyfill';
-
 // vue
-window.Vue = require('vue');
+window.Vue = require('vue')
 
 // iview
-import iView from 'iview';
+import iView from 'view-design'
 
-Vue.use(iView);
+Vue.use(iView)
 
-// bootstrap
-require('./bootstrap');
+// axios
+import axios from 'axios'
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+let token = document.head.querySelector('meta[name="csrf-token"]')
+
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+
+    Vue.prototype.$csrf_token = token.content
+}
+
+iView.LoadingBar.config({
+    height: 3
+})
+
+axios.interceptors.request.use((config) => {
+    iView.LoadingBar.start()
+
+    return config
+}, function (error) {
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use((response) => {
+    iView.LoadingBar.finish()
+
+    return response
+}, function (error) {
+    if (error.response.status === 419) {
+        window.location.reload()
+    } else {
+        iView.LoadingBar.error()
+
+        iView.Message.error({
+            background: true,
+            content: '这是一条带背景色的通知'
+        })
+
+        // let msg = _.defaultTo(error.response.data.message) || _.defaultTo(error.response.statusText) || error.response.status
+    }
+
+    return Promise.reject(error)
+})
+
+Vue.prototype.$http = axios
+
+// CDN
+Vue.prototype.$cdn = process.env.MIX_APP_CDN
 
 // components
-require('./components');
+require('./components')
 
 // app
 const app = new Vue({
     el: '#app'
-});
+})
